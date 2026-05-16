d8e3b6b42dddd745ce1e1a0e825b8f29471f69b5
DIO0550 <47467473+DIO0550@users.noreply.github.com>
2026-05-17 00:45:43 +0900

    update

---
+++ meet-log/meet-log/meet_logApp.swift
@@ -0,0 +1,17 @@
+//
+//  meet_logApp.swift
+//  meet-log
+//
+//  Created by DIO on 2026/05/16.
+//
+
+import SwiftUI
+
+@main
+struct meet_logApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ meet-log/meet-logTests/meet_logTests.swift
@@ -0,0 +1,19 @@
+//
+//  meet_logTests.swift
+//  meet-logTests
+//
+//  Created by DIO on 2026/05/16.
+//
+
+import Testing
+@testable import meet_log
+
+struct meet_logTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
